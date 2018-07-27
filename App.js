d283e424efd915b6c3653584b3ee2c611c856f4f
Alber Erre <arblugo@gmail.com>
2018-07-27 16:44:25 +0100

    create-react-native-init

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <View style={styles.container}>
+          <Text style={styles.textStyle}>Testing App</Text>
+          <Text>How are youu mate?</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scroll: {
+    flexGrow : 1,
+    justifyContent : 'center'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    color: '#FF99FF',
+    fontSize: 28
+  }
+});
