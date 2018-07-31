42c7e8e116510f8d16c09ea22e050debaa4cb722
Alber Erre <arblugo@gmail.com>
2018-07-31 12:07:09 +0100

    camera + map components

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { RNCamera, FaceDetector } from 'react-native-camera';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
